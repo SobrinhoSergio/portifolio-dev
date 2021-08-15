25c33b227a0aadbb43052ba3592539bc3df33e2d
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2021-08-15 14:00:03 -0300

    atualizando segurança

--- contato.php
+++ contato.php
@@ -8,7 +8,7 @@
             <h2>Formulário de Contato</h2>
             
             <form 
-                action="https://formsubmit.co/sobrinhosergio00@gmail.com"   
+                action="3584e121c5d1098f90fcac0b11bfbd5e"   
                 method="POST" 
                 class="form"
             >
